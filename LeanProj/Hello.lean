51de9dd730f062d9b8fec85b18b2768ec7f48280
Satoshi OKUI <okuisatoshi@mac.com>
2026-04-06 10:17:58 +0900

    #version

--- LeanProj/Hello.lean
+++ LeanProj/Hello.lean
@@ -1,8 +1,14 @@
 import Mathlib
 
+#version
+
 example (a b : Nat) :
   (a + b) * (a + b) = a * a + 2 * a * b + b * b := by ring
 
 def square (n : Nat) := n * n
 
+#check square
+
 #eval 2 |> square |> square |> square
+
+
